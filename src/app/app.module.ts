99bd46526e443abf46ed0703d2a3e82a0ff184f4
safaa_ali <safaaaly246@gmail.com>
2020-10-04 19:09:11 +0200

    built simple department UI with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { MainComponent } from './main/main.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { DepartmentComponent } from './department/department.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SideBarComponent,
+    MainComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    TooltipModule,
+    MatSliderModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
+
+       ],
   providers: [],
   bootstrap: [AppComponent]
 })
